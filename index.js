b1f6673b73f7a68ad4ef50e9c9efb01170afa0ca
jessilin007@gmail.com <jessilin007@gmail.com>
2026-02-06 19:13:43 +0100

    start service

--- index.js
+++ index.js
@@ -16,5 +16,17 @@ const messages = [
     text:"Hello world!",
     usr:"Charles",
     added:new Date()
+    
   }
 ]
+
+
+app.get("/",(req,res) =>{
+  res.render("index", { title: "Mini Message Board", messages: messages });
+})
+
+
+const PORT = 30000;
+app.listen(PORT, () =>{
+  console.log(`Server is running on port ${PORT}`);
+})
