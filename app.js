3dde7d13ddeda9c7193ac028eda43a0b6d87973a
jessilin007@gmail.com <jessilin007@gmail.com>
2026-02-07 19:20:37 +0100

    complete basic logic,start Css styling

---
+++ app.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+
+const express = require("express");
+const indexRouter = require('./routes/indexRouter');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+//views
+app.set("views",path.join(__dirname,"views"));
+app.set('view engine', 'ejs');
+
+//middleware
+app.use(express.urlencoded({ extended:true}));
+app.use(express.json());
+
+//static files
+const assetsPath = path.join(__dirname,"Assets");
+app.use(express.static(assetsPath));
+
+//routes
+app.use("/",indexRouter);
+
+app.listen(port,() =>{
+  console.log(`Serve is running on port ${ port}`);
+})
+
+
---
+++ routes/indexRouter.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const router = express.Router();
+
+
+const messages =[
+  {
+    text:"Hi there!",
+    user:"Amando",
+    added:new Date()
+    },
+    {
+      text:"Hello,world!",
+      user:"Charles",
+      added:new Date()
+    }
+]
+
+
+router.get('/',(req,res) => {
+  res.render('index',{
+    title:"BRIANNA  Messageboard",
+    messages:messages
+  });
+})
+router.get("/new",(req,res)=>{
+  res.render("form")
+})
+
+router.get("/message/:id", (req,res) =>{
+  const messageId = req.params.id;
+  const message = messages[messageId];
+
+  if(message){
+    res.render("messageDetail",{
+      title:"Message Details",
+      message:message,
+
+    });
+  }else{
+    res.status(404).send("Message not found ")
+  }
+})
+
+router.post('/add-message',(req,res)=>{
+  const newMessage = {
+    user:req.body.username,
+    text:req.body.content,
+    added:new Date()
+  };
+  messages.unshift(newMessage);
+  res.redirect('/')
+});
+
+router.post('/new',(req,res) =>{
+  const userName = req.body.messageUser;
+  const userText = req.body.messageText;
+  
+  console.log(req.body);
+
+  messages.push({ 
+    text:userText , 
+    user:userName, 
+    added:new Date()
+  })
+
+  res.redirect('/');
+})
+
+module.exports = router;
