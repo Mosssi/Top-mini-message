df172ade664979076c1f5e355e478c6088cafd4b
jessilin007@gmail.com <jessilin007@gmail.com>
2026-02-08 01:05:15 +0100

    replace separate pages with modals for creating and viewing messages

---
+++ public/JS/script.js
@@ -0,0 +1,20 @@
+const modal = document.getElementById('myModal');
+const btn = document.getElementById("myBtn");
+const span = document.getElementsByClassName("close")[0];
+
+if(btn){
+  btn.onclick = function(){
+    modal.style.display ="block";
+  }
+}
+if(span){
+  span.onclick = function(){
+    modal.style.display ="none";
+  }
+}
+
+window.onclick = function(event){
+  if(event.target == modal){
+    modal.style.display ="none";
+  }
+}
