11bb90c310459d1f16267be36b0d5bd10dcaacc2
jessilin007@gmail.com <jessilin007@gmail.com>
2026-02-08 01:15:14 +0100

    add open modal logic

--- public/JS/script.js
+++ public/JS/script.js
@@ -18,3 +18,41 @@ window.onclick = function(event){
     modal.style.display ="none";
   }
 }
+
+const detailModal = document.getElementById('detailModal');
+const detailClose = document.getElementsByClassName("close-detail")[0];
+const openBtn = document.querySelectorAll('.open-btn');
+
+openBtn.forEach(btn => {
+  btn.onclick = function(e){
+    e.preventDefault();
+    
+    const user = this.getAttribute('data-user');
+    const text = this.getAttribute('data-text');
+    const date = this.getAttribute('data-date');
+
+    document.getElementById('detailUser').innerText = user;
+    document.getElementById('detailText').innerText = text;
+    document.getElementById('detailDate').innerText = date;
+
+    detailModal.style.display = "block"
+
+  }
+});
+
+if(detailClose){
+  detailClose.onclick = function(){
+    detailClose.onclick = function(){
+      detailModal.style.display = "none";
+    }
+  }
+
+  window.onclick = function(event){
+    if(event.target == writeModal){
+      writeModal.style.display = "none";
+    }
+    if(event.target == detailModal) {
+      detailModal.style.display ="none";
+    }
+  }
+}
